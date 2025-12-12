13d155c2164b5caa4e58d6bb0ac47beab0edf639
Ingemarsson Oskar <oskar@oskar.me>
2025-12-13 00:24:23 +0100

    consider more possible errors

--- src/network/error.rs
+++ src/network/error.rs
@@ -6,8 +6,21 @@ pub enum ReqwlessError {
 impl defmt::Format for ReqwlessError {
     fn format(&self, fmt: defmt::Formatter<'_>) {
         match self {
+            Self::Error(reqwless::Error::Dns) => defmt::write!(fmt, "{}", "Dns"),
             Self::Error(reqwless::Error::Network(_)) => defmt::write!(fmt, "{}", "Network"),
-            Self::Error(_) => defmt::write!(fmt, "{}", "Other"),
+            Self::Error(reqwless::Error::Codec) => defmt::write!(fmt, "{}", "Codec"),
+            Self::Error(reqwless::Error::InvalidUrl(_)) => defmt::write!(fmt, "{}", "InvalidUrl"),
+            Self::Error(reqwless::Error::Tls(_)) => defmt::write!(fmt, "{}", "Tls"),
+            Self::Error(reqwless::Error::BufferTooSmall) => {
+                defmt::write!(fmt, "{}", "BufferTooSmall")
+            }
+            Self::Error(reqwless::Error::AlreadySent) => defmt::write!(fmt, "{}", "AlreadySent"),
+            Self::Error(reqwless::Error::IncorrectBodyWritten) => {
+                defmt::write!(fmt, "{}", "IncorrectBodyWritten")
+            }
+            Self::Error(reqwless::Error::ConnectionAborted) => {
+                defmt::write!(fmt, "{}", "ConnectionAborted")
+            }
         }
     }
 }
