643d80b542d0a2367f49aa3b57d6c8ea1d86279e
Ingemarsson Oskar <oskar@oskar.me>
2025-12-14 18:27:46 +0100

    started to migrate the server to axum

---
+++ axum-server/src/main.rs
@@ -0,0 +1,20 @@
+use axum::{Router, routing::get};
+use tokio::signal::unix::{SignalKind, signal};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:5001").await.unwrap();
+
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
+}
+
+async fn shutdown_signal() {
+    let mut sigterm = signal(SignalKind::terminate()).expect("failed to install signal handler");
+    sigterm.recv().await;
+    println!("SIGTERM received, shutting down...");
+}
