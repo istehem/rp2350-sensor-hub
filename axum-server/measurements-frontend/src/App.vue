cf074839c11d84f538be48d6d66b18ade4205c26
Ingemarsson Oskar <oskar@oskar.me>
2026-01-11 15:30:49 +0100

    circular progress

--- axum-server/measurements-frontend/src/App.vue
+++ axum-server/measurements-frontend/src/App.vue
@@ -38,18 +38,19 @@ onMounted(async () => {
 <template>
   <header class="transparent"></header>
   <main class="responsive">
-    <article class="center-align" v-if="apiError">
-      <h6>{{ apiError.message }}</h6>
-    </article>
-    <article class v-else-if="measurement">
-      <div class="center-align">
+    <article class="small center-align">
+      <div class="center-align" v-if="apiError">
+        <h6>{{ apiError.message }}</h6>
+      </div>
+      <div v-else-if="measurement">
         <h6>Measured at: {{ new Date(measurement.date).toLocaleString() }}</h6>
         <h6>Temperature: {{ measurement.temperature }}°C</h6>
         <h6>Humidity: {{ measurement.humidity }}%</h6>
       </div>
+      <div v-else>
+        <progress class="circle small indeterminate" value="50" max="100"></progress>
+      </div>
     </article>
-
-    <article v-else><p>Loading...</p></article>
   </main>
   <footer class="transparent"></footer>
 </template>
