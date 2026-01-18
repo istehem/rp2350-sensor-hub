e8c1ca52436c6534ccafb4d5ddbeebffab5b5df7
Ingemarsson Oskar <oskar@oskar.me>
2026-01-18 13:17:57 +0100

    use the API controller to get the latest measurement

--- axum-server/measurements-frontend/src/App.vue
+++ axum-server/measurements-frontend/src/App.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted } from 'vue'
 
-import config from './config.ts'
 import type { ApiError, Measurement } from './assets.ts'
 import TemperatureChart from './charts/TemperatureChart.vue'
 import HumidityChart from './charts/HumidityChart.vue'
 import ErrorPanel from './ErrorPanel.vue'
-import { getErrorMessage, toMeasurement } from './utils.ts'
-import { fetchMeasurements } from './measurementsApi.ts'
+import { fetchLatestMeasurement, fetchMeasurements } from './measurementsApi.ts'
 
 const latestMeasurement = ref<Measurement | null>(null)
 const measurements = ref<Measurement[] | null>(null)
 const measurementsApiError = ref<ApiError | null>(null)
-const apiError = ref<ApiError | null>(null)
+const latestMeasurementApiError = ref<ApiError | null>(null)
 const switchModeIcon = ref<string>('dark_mode')
-var intervalId: number | null = null
+var latestMeasurementIntervalId: number | null = null
+var measurementsIntervalId: number | null = null
 
 async function toggleSwitchModeIcon() {
   const mode = await ui('mode')
@@ -37,34 +36,35 @@ async function flipMode() {
 
 onMounted(async () => {
   toggleSwitchModeIcon()
-  const measurementsResponse = await fetchMeasurements()
-  if (measurementsResponse._kind === 'ApiError') {
-    measurementsApiError.value = measurementsResponse
-  } else {
-    measurements.value = measurementsResponse.measurements
-  }
 
-  const fetchMeasurement = async () => {
-    try {
-      const response = await fetch(`${config.apiHost}/api/measurements/latest`)
-      if (response.ok) {
-        const data = await response.json()
-        latestMeasurement.value = toMeasurement(data)
-        apiError.value = null
-      } else {
-        apiError.value = await response.json()
-      }
-    } catch (error) {
-      apiError.value = { _kind: 'ApiError', message: getErrorMessage(error) }
-      console.error('Fetch failed:', error)
+  const updateMeasurements = async () => {
+    const measurementsResponse = await fetchMeasurements()
+    if (measurementsResponse._kind === 'ApiError') {
+      measurementsApiError.value = measurementsResponse
+    } else {
+      measurements.value = measurementsResponse.measurements
     }
   }
-  fetchMeasurement()
-  intervalId = setInterval(fetchMeasurement, 10000)
+
+  const updateLatestMeasurement = async () => {
+    const measurementResponse = await fetchLatestMeasurement()
+    if (measurementResponse._kind === 'ApiError') {
+      latestMeasurementApiError.value = measurementResponse
+    } else {
+      latestMeasurement.value = measurementResponse
+    }
+  }
+
+  updateLatestMeasurement()
+  latestMeasurementIntervalId = setInterval(updateLatestMeasurement, 10000)
+
+  updateMeasurements()
+  measurementsIntervalId = setInterval(updateMeasurements, 60000)
 })
 
 onUnmounted(() => {
-  if (intervalId) clearInterval(intervalId)
+  if (latestMeasurementIntervalId) clearInterval(latestMeasurementIntervalId)
+  if (measurementsIntervalId) clearInterval(measurementsIntervalId)
 })
 </script>
 
@@ -78,7 +78,7 @@ onUnmounted(() => {
     </nav>
   </header>
   <main class="responsive">
-    <ErrorPanel :error="apiError" v-if="apiError" />
+    <ErrorPanel :error="latestMeasurementApiError" v-if="latestMeasurementApiError" />
     <div v-else-if="latestMeasurement">
       <article>
         <div class="grid shrink-center">
--- axum-server/measurements-frontend/src/measurementsApi.ts
+++ axum-server/measurements-frontend/src/measurementsApi.ts
@@ -1,4 +1,4 @@
-import type { ApiError, Measurements } from './assets.ts'
+import type { ApiError, Measurement, Measurements } from './assets.ts'
 import config from './config.ts'
 import { getErrorMessage, toMeasurement } from './utils.ts'
 
@@ -8,6 +8,23 @@ export async function fetchMeasurements(): Promise<Measurements | ApiError> {
     if (response.ok) {
       const data = await response.json()
       return { _kind: 'Measurements', measurements: data.map(toMeasurement) }
+    } else {
+      const apiError: ApiError = await response.json()
+      return apiError
+    }
+  } catch (error) {
+    const apiError: ApiError = { _kind: 'ApiError', message: getErrorMessage(error) }
+    console.error('Fetch failed:', error)
+    return apiError
+  }
+}
+
+export async function fetchLatestMeasurement(): Promise<Measurement | ApiError> {
+  try {
+    const response = await fetch(`${config.apiHost}/api/measurements/latest`)
+    if (response.ok) {
+      const data = await response.json()
+      return toMeasurement(data)
     } else {
       const apiError: ApiError = await response.json()
       return apiError
