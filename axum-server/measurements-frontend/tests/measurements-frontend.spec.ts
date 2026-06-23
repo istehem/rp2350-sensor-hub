dc282761a4fcd8f3cb8c302ddd63cda864d5565a
Ingemarsson Oskar <oskar@oskar.me>
2026-06-23 23:01:45 +0200

    started mocking rest calls in the integration tests

--- axum-server/measurements-frontend/tests/measurements-frontend.spec.ts
+++ axum-server/measurements-frontend/tests/measurements-frontend.spec.ts
@@ -10,3 +10,41 @@ test('has navigation', async ({ page }) => {
   await page.goto(config.homeUrl);
   await expect(page.locator('nav').getByText('Measurements')).toBeVisible();
 });
+
+test('mock latest measurement success', async ({ page }) => {
+  const temperature = 33.0;
+  const humidity = 33.33;
+  await page.route('**/api/measurements/latest', async (route) => {
+    await route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      body: JSON.stringify([
+        { "date": "2026-06-23T20:38:43.688746298Z", "temperature": temperature, "humidity": humidity }
+      ])
+    });
+  });
+
+  await page.goto(config.homeUrl);
+
+  await expect(page.locator('article').getByText(temperature.toString())).toBeVisible();
+  await expect(page.locator('article').getByText(humidity.toString())).toBeVisible();
+});
+
+test('mock latest measurement error', async ({ page }) => {
+  const error = "this is a server error";
+  await page.route('**/api/measurements/latest', async (route) => {
+    await route.fulfill({
+      status: 500,
+      contentType: 'application/json',
+      body: JSON.stringify([
+        { "message": error }
+      ])
+    });
+  });
+
+  await page.goto(config.homeUrl);
+
+  await expect(page.locator('article[class*="error"]').getByText(error)).toBeVisible();
+  await expect(page.getByText('Temperature')).toHaveCount(0);
+  await expect(page.getByText('Humidity')).toHaveCount(0);
+});
