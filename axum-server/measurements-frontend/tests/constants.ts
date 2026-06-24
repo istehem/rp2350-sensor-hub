a5eb237897288ca4835af4000d84534a913d2773
Ingemarsson Oskar <oskar@oskar.me>
2026-06-24 23:24:47 +0200

    test that images are rendered when measurements are retrieved successfully

--- axum-server/measurements-frontend/tests/constants.ts
+++ axum-server/measurements-frontend/tests/constants.ts
@@ -1,4 +1,4 @@
 export const config = {
   homeUrl: 'http://localhost:5173',
   apiHost: 'http://localhost:5173',
-};
+}
--- axum-server/measurements-frontend/tests/measurements-frontend.spec.ts
+++ axum-server/measurements-frontend/tests/measurements-frontend.spec.ts
@@ -1,50 +1,69 @@
-import { test, expect } from '@playwright/test';
-import { config } from './constants';
+import { test, expect } from '@playwright/test'
+import { config } from './constants'
 
 test('has title', async ({ page }) => {
-  await page.goto(config.homeUrl);
-  await expect(page).toHaveTitle(/Measurements/);
-});
+  await page.goto(config.homeUrl)
+  await expect(page).toHaveTitle(/Measurements/)
+})
 
 test('has navigation', async ({ page }) => {
-  await page.goto(config.homeUrl);
-  await expect(page.locator('nav').getByText('Measurements')).toBeVisible();
-});
+  await page.goto(config.homeUrl)
+  await expect(page.locator('nav').getByText('Measurements')).toBeVisible()
+})
 
 test('mock latest measurement success', async ({ page }) => {
-  const temperature = 33.1;
-  const humidity = 33.2;
+  const temperature = 33.1
+  const humidity = 33.2
   await page.route('**/api/measurements/latest', async (route) => {
     await route.fulfill({
       status: 200,
       contentType: 'application/json',
-      body: JSON.stringify(
-        { "date": "2026-06-23T20:38:43.688746298Z", "temperature": temperature, "humidity": humidity }
-      )
-    });
-  });
+      body: JSON.stringify({
+        date: '2026-06-23T20:38:43.688746298Z',
+        temperature: temperature,
+        humidity: humidity,
+      }),
+    })
+  })
 
-  await page.goto(config.homeUrl);
+  await page.goto(config.homeUrl)
 
-  await expect(page.locator('article').getByText(`${temperature}°C`, { exact: true })).toBeVisible();
-  await expect(page.locator('article').getByText(`${humidity}%`, { exact: true })).toBeVisible();
-});
+  await expect(page.locator('article').getByText(`${temperature}°C`, { exact: true })).toBeVisible()
+  await expect(page.locator('article').getByText(`${humidity}%`, { exact: true })).toBeVisible()
+})
 
 test('mock latest measurement error', async ({ page }) => {
-  const error = "this is a server error";
+  const error = 'this is a server error'
   await page.route('**/api/measurements/latest', async (route) => {
     await route.fulfill({
       status: 500,
       contentType: 'application/json',
-      body: JSON.stringify(
-        { "message": error }
-      )
-    });
-  });
+      body: JSON.stringify({ message: error }),
+    })
+  })
 
-  await page.goto(config.homeUrl);
+  await page.goto(config.homeUrl)
 
-  await expect(page.locator(`article:has(:text-is("${error}"))`)).toBeVisible();
-  await expect(page.getByText('Temperature')).toHaveCount(0);
-  await expect(page.getByText('Humidity')).toHaveCount(0);
-});
+  await expect(page.locator(`article:has(:text-is("${error}"))`)).toBeVisible()
+  await expect(page.getByText('Temperature')).toHaveCount(0)
+  await expect(page.getByText('Humidity')).toHaveCount(0)
+})
+
+test('mock measurements success', async ({ page }) => {
+  const temperature = 33.1
+  const humidity = 33.2
+  await page.route(/\/api\/measurements(\?.*)?$/, async (route) => {
+    await route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      body: JSON.stringify([
+        { date: '2026-06-23T20:38:43.688746298Z', temperature: temperature, humidity: humidity },
+        { date: '2026-06-23T20:38:53.688746298Z', temperature: temperature, humidity: humidity },
+        { date: '2026-06-23T20:39:03.688746298Z', temperature: temperature, humidity: humidity },
+      ]),
+    })
+  })
+
+  await page.goto(config.homeUrl)
+  await expect(page.locator('article canvas[role=img]')).toHaveCount(2)
+})
