6b8c33dd337b8beebfc6bab5c6a4bc9f5fae80e1
Ingemarsson Oskar <oskar@oskar.me>
2026-08-01 23:12:20 +0200

    tests can compile the main crate on amd64

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,8 @@
 #![no_std]
-extern crate alloc;
 
 use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 use embassy_sync::channel::Channel;
 use serde::Serialize;
-
-pub mod game {
-    mod cache;
-    mod entities;
-    mod error;
-    mod player;
-    pub mod tasks;
-}
-
-pub mod network {
-    pub mod controller;
-    mod error;
-}
-
-#[cfg(feature = "temperature")]
-pub mod temperature_and_humidity {
-    mod error;
-    pub mod tasks;
-    pub use embassy_rp::peripherals::PIO0;
-}
 
 #[derive(Clone, Serialize)]
 pub struct Measurement {
--- src/temperature_and_humidity/tasks.rs
+++ src/temperature_and_humidity/tasks.rs
@@ -8,7 +8,7 @@ use embassy_rp::{
 use embassy_time::Timer;
 
 use crate::temperature_and_humidity::error::FormattableDHTSensorError;
-use crate::{Measurement, TempHumidityChannel};
+use rp2350_sensor_hub::{Measurement, TempHumidityChannel};
 
 type Pio = PIO0;
 type DHTStateMachine = StateMachine<'static, Pio, 0>;
