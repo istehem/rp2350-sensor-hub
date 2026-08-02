26dc4b6ebd562bf6304fb25c5b09a2bde81c5fce
Ingemarsson Oskar <oskar@oskar.me>
2026-08-02 23:41:39 +0200

    started testing graphics in CI

--- tests/test_game.rs
+++ tests/test_game.rs
@@ -4,7 +4,7 @@ include!("common/defmt_mock.rs");
 mod tests {
     use core::convert::Infallible;
     use embedded_graphics::{pixelcolor::BinaryColor, prelude::*};
-    use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Window};
+    use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay};
     use game_logic::two_four_eighteen::Game;
     use rand::SeedableRng;
     use rand::rngs::SmallRng;
@@ -15,44 +15,26 @@ mod tests {
     const SCREEN_WIDTH: u32 = SCALE * 128;
     const SCREEN_HEIGHT: u32 = SCALE * 64;
 
-    use lazy_static::lazy_static;
-    use std::sync::Mutex;
-
-    lazy_static! {
-        static ref TEST_MUTEX: Mutex<()> = Mutex::new(());
-    }
-
     type Display = SimulatorDisplay<BinaryColor>;
-
-    fn draw_in_window(display: &Display) -> Result<(), Infallible> {
-        let output_settings = OutputSettingsBuilder::new().scale(1).build();
-        Window::new("a die", &output_settings).show_static(&display);
-
-        Ok(())
-    }
 
     #[fixture]
     fn init_display() -> Display {
         SimulatorDisplay::new(Size::new(SCREEN_WIDTH, SCREEN_HEIGHT))
     }
 
-    #[fixture]
-    fn gen_small_rng() -> SmallRng {
-        let seed: u64 = rand::random();
-        SmallRng::seed_from_u64(seed)
-    }
-
     #[rstest]
     #[test_log::test]
-    fn play_and_draw(
-        #[from(init_display)] mut display: Display,
-        #[from(gen_small_rng)] small_rng: SmallRng,
-    ) -> Result<(), Infallible> {
-        let _guard = TEST_MUTEX.lock().unwrap();
-        let mut game = Game::new(small_rng);
+    fn play_and_draw(#[from(init_display)] mut display: Display) -> Result<(), Infallible> {
+        let seed: u64 = 2056713228146178055;
+        let mut game = Game::new(SmallRng::seed_from_u64(seed));
+        let output_settings = OutputSettingsBuilder::new().build();
 
         game::player::play_and_draw(&mut display, &mut game).unwrap();
+        let output_image = display.to_grayscale_output_image(&output_settings);
 
-        draw_in_window(&display)
+        //let expected =
+
+        assert_eq!(output_image.size(), Size::new(SCREEN_WIDTH, SCREEN_HEIGHT));
+        Ok(())
     }
 }
