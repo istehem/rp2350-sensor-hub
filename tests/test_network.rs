0998ae844a98e69fe9fab82ad3f922c5b55d57c2
Ingemarsson Oskar <oskar@oskar.me>
2026-08-02 15:09:46 +0200

    test that the right body, content type and credentials are set

--- tests/test_network.rs
+++ tests/test_network.rs
@@ -44,15 +44,20 @@ mod tests {
     use rstest::rstest;
     use static_cell::StaticCell;
     use std_embedded_nal_async::Stack;
-
-    use wiremock::matchers::{method, path};
+    use wiremock::matchers::{basic_auth, body_json, header, method, path};
     use wiremock::{Mock, MockServer, ResponseTemplate};
 
     static TEMP_HUMIDITY_CHANNEL: StaticCell<TempHumidityChannel> = StaticCell::new();
 
-    async fn mock_measurements(mock_server: &MockServer) {
+    const REST_USER: &str = env!("REST_USER");
+    const REST_USER_PASSWORD: &str = env!("REST_USER_PASSWORD");
+
+    async fn mock_measurements(mock_server: &MockServer, measurement: &Measurement) {
         Mock::given(method("POST"))
+            .and(basic_auth(REST_USER, REST_USER_PASSWORD))
+            .and(header("Content-Type", "application/json"))
             .and(path("/api/measurements"))
+            .and(body_json(measurement))
             .respond_with(ResponseTemplate::new(201))
             .mount(&mock_server)
             .await;
@@ -63,17 +68,18 @@ mod tests {
     #[test_log::test]
     async fn network() -> Result<(), SendMeasurementError> {
         let mock_server = MockServer::start().await;
-        mock_measurements(&mock_server).await;
+
+        let measurement = Measurement {
+            temperature: 25.0,
+            humidity: 45.0,
+        };
+        mock_measurements(&mock_server, &measurement).await;
 
         let endpoint = format!("{}/api/measurements", mock_server.uri());
 
         let stack = Stack::default();
         let mut client = HttpClient::new(&stack, &stack);
         let channel = TEMP_HUMIDITY_CHANNEL.init(Channel::new());
-        let measurement = Measurement {
-            temperature: 25.0,
-            humidity: 45.0,
-        };
         channel.send(measurement).await;
         let status_code = api::post_measurement(&mut client, endpoint.as_str(), channel).await?;
 
