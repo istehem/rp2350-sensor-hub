13f2a6d145b34a9b2da5395936e4c449cf9494ca
Ingemarsson Oskar <oskar@oskar.me>
2026-08-03 20:47:43 +0200

    test that the expected image is generated

--- tests/test_game.rs
+++ tests/test_game.rs
@@ -2,12 +2,11 @@ include!("common/defmt_mock.rs");
 
 #[cfg(test)]
 mod tests {
-    use core::convert::Infallible;
     use embedded_graphics::{pixelcolor::BinaryColor, prelude::*};
     use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay};
     use game_logic::two_four_eighteen::Game;
-    use rand::rngs::SmallRng;
     use rand::SeedableRng;
+    use rand::rngs::SmallRng;
     use rp2350_sensor_hub::game;
     use rstest::{fixture, rstest};
 
@@ -23,18 +22,21 @@ mod tests {
     }
 
     #[rstest]
+    #[case::seed_17035409315052165818(17035409315052165818)]
+    #[case::seed_2056713228146178055(2056713228146178055)]
     #[test_log::test]
-    fn play_and_draw(#[from(init_display)] mut display: Display) -> Result<(), Infallible> {
-        let seed: u64 = 2056713228146178055;
-        let mut game = Game::new(SmallRng::seed_from_u64(seed));
+    fn play_and_draw(#[from(init_display)] mut display: Display, #[case] seed: u64) {
         let output_settings = OutputSettingsBuilder::new().build();
+        let mut game = Game::new(SmallRng::seed_from_u64(seed));
 
         game::player::play_and_draw(&mut display, &mut game).unwrap();
-        let output_image = display.to_grayscale_output_image(&output_settings);
+        let generated_roll_image = display.to_grayscale_output_image(&output_settings);
 
-        //let expected =
+        let path_to_expected_roll_image = format!("resources/roll_{}.png", seed);
+        let expected_roll_image = Display::load_png(path_to_expected_roll_image)
+            .unwrap()
+            .to_grayscale_output_image(&output_settings);
 
-        assert_eq!(output_image.size(), Size::new(SCREEN_WIDTH, SCREEN_HEIGHT));
-        Ok(())
+        assert_eq!(generated_roll_image, expected_roll_image);
     }
 }
