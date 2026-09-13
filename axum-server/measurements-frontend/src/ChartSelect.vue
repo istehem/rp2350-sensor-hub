5e59d9e340828041b3df5e3c1170eaec236acd97
Ingemarsson Oskar <oskar@oskar.me>
2026-09-13 21:17:10 +0200

    emit the selected value directly

--- axum-server/measurements-frontend/src/ChartSelect.vue
+++ axum-server/measurements-frontend/src/ChartSelect.vue
@@ -11,25 +11,17 @@ const emit = defineEmits<{
   chartSelectionChanged: [ChartSelectMode]
 }>()
 
-const onMedianBandSelected = () => {
-  emit('chartSelectionChanged', ChartSelectMode.MedianAndBand)
-}
-const onDecimationSelected = () => {
-  emit('chartSelectionChanged', ChartSelectMode.Decimation)
-}
-
 const selected = ref<ChartSelectMode>(properties.initState)
 </script>
 <template>
   <fieldset>
-    <nav>
+    <nav @change="emit('chartSelectionChanged', selected)">
       <label class="radio">
         <input
           v-model="selected"
           type="radio"
           :name="properties.name"
           :value="ChartSelectMode.MedianAndBand"
-          @change="onMedianBandSelected"
         />
         <span>Median And Band</span>
       </label>
@@ -39,7 +31,6 @@ const selected = ref<ChartSelectMode>(properties.initState)
           type="radio"
           :name="properties.name"
           :value="ChartSelectMode.Decimation"
-          @change="onDecimationSelected"
         />
         <span>Decimation</span>
       </label>
